851084f0d935decfd2e334622b4b24c73828d628
Krishmi <kuhkd150@gmail.com>
2025-10-01 19:38:24 +0530

    Update App.jsx

--- ev-web/src/App.jsx
+++ ev-web/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -53,5 +54,65 @@ export default function App() {
       </button>
     </div>
   );
+=======
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AppLayout from './components/AppLayout'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Owners from './pages/Owners'
+import OwnerForm from './pages/OwnerForm'
+import Stations from './pages/Stations'
+import StationForm from './pages/StationForm'
+import Bookings from './pages/Bookings'
+import BookingForm from './pages/BookingForm'
+import BookingQR from './pages/BookingQR'
+import Users from './pages/Users'
+import RequireRole from './auth/RequireRole'
+import { Toaster } from 'react-hot-toast'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={
+          <RequireRole roles={['Backoffice','Operator']}>
+            <AppLayout/>
+          </RequireRole>
+        }>
+          <Route index element={<Dashboard/>} />
+
+          <Route path="users" element={
+            <RequireRole roles={['Backoffice']}><Users/></RequireRole>
+          }/>
+
+          <Route path="owners" element={
+            <RequireRole roles={['Backoffice']}><Owners/></RequireRole>
+          }/>
+          <Route path="owners/new" element={
+            <RequireRole roles={['Backoffice']}><OwnerForm/></RequireRole>
+          }/>
+          <Route path="owners/:nic" element={
+            <RequireRole roles={['Backoffice']}><OwnerForm/></RequireRole>
+          }/>
+
+          <Route path="stations" element={<Stations/>} />
+          <Route path="stations/new" element={
+            <RequireRole roles={['Backoffice']}><StationForm/></RequireRole>
+          }/>
+          <Route path="stations/:id" element={
+            <RequireRole roles={['Backoffice']}><StationForm/></RequireRole>
+          }/>
+
+          <Route path="bookings" element={<Bookings/>} />
+          <Route path="bookings/new" element={<BookingForm/>} />
+          <Route path="bookings/:id" element={<BookingForm/>} />
+          <Route path="bookings/:id/qr" element={<BookingQR/>} />
+        </Route>
+      </Routes>
+      <Toaster/>
+    </BrowserRouter>
+  )
+>>>>>>> Stashed changes
 }
 
