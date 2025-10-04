fd188253e13769f366bdfaba9585c3a65fff0b41
chamodi <chamodipiyadasa@gmail.com>
2025-10-04 09:53:42 +0530

    update the auth endpoint path to /api/auth/login and make the axios client only send an Authorization header

--- ev-web/src/api/auth.js
+++ ev-web/src/api/auth.js
@@ -1,6 +1,7 @@
 import api from "./client";
 
+// server expects /api/auth/login
 export const login = async (username, password) => {
-  const { data } = await api.post("/auth/login", { username, password });
+  const { data } = await api.post("/api/auth/login", { username, password });
   return data;
 };
